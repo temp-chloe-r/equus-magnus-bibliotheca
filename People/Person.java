b4456b5383536967d0f3eed38abc08b66b050822
Stamatis Theocharous <StamatisTheocharous@gmail.com>
2024-10-25 10:38:08 +0100

    Update Person

--- People/Person.java
+++ People/Person.java
@@ -3,51 +3,60 @@ package People;
 import java.time.LocalDate;
 
 public class Person {
-    private String FirstName;
-    private String LastName;
-    private LocalDate Dob;
+    private String FirstName; // First name of the person
+    private String LastName; // Last name of the person
+    private LocalDate Dob; // Date of birth of the person
+    private static int PeopleCount = 0; // Static variable to count the number of people
 
-    private static int PeopleCount = 0;
-
-    public Person(String FirstName, String LastName, LocalDate Dob){
+    // Constructor to initialize the person's details and increment the people count
+    public Person(String FirstName, String LastName, LocalDate Dob) {
         this.FirstName = FirstName;
         this.LastName = LastName;
         this.Dob = Dob;
-        PeopleCount ++;
+        PeopleCount++;
     }
 
+    // Getter for the first name
     public String getFirstName() {
         return FirstName;
     }
 
+    // Setter for the first name
     public void setFirstName(String firstName) {
         FirstName = firstName;
     }
 
+    // Getter for the last name
     public String getLastName() {
         return LastName;
     }
 
+    // Setter for the last name
     public void setLastName(String lastName) {
         LastName = lastName;
     }
 
+    // Getter for the date of birth
     public LocalDate getDob() {
         return Dob;
     }
 
+    // Setter for the date of birth
     public void setDob(LocalDate dob) {
         this.Dob = Dob;
     }
 
+    // Static method to get the count of people
     public static int getPeopleCount() {
         return PeopleCount;
     }
 
+    // Static method to set the count of people
     public static void setPeopleCount(int peopleCount) {
         PeopleCount = peopleCount;
     }
 
+    // Override toString method to display person's details
     @Override
     public String toString() {
         return "Person{" +
@@ -57,11 +66,12 @@ public class Person {
                 ", PeopleCount=" + getPeopleCount() +
                 '}';
     }
+
     public static void main(String[] args) {
-        // Creating instances of MyClass
+        // Creating instances of Person
         Person scott = new Person("Scott", "O' Brien", LocalDate.of(1999, 1, 11));
         Person chloe = new Person("Chloe", "Please add", LocalDate.of(1999, 3, 23));
-        Person sraddheya = new Person("sraddheya", "Please add", LocalDate.of(1999, 1, 11));
+        Person sraddheya = new Person("Sraddheya", "Please add", LocalDate.of(1999, 1, 11));
         Person tina = new Person("Avgustina", "Pinaldi", LocalDate.of(1995, 3, 9));
         Person stam = new Person("Stamatis", "Theocharous", LocalDate.of(1999, 5, 28));
 
@@ -69,11 +79,4 @@ public class Person {
         System.out.println("Number of people created: " + Person.getPeopleCount());
         System.out.println(scott.toString());
     }
-
-
 }
-
-
-
-
-
